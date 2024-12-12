0344cb7710f5c0d44b9987b2fbedcfc17521367d
Valery Kotov <kotov.valery@gmail.com>
2024-12-12 10:33:03 +0100

    Add SearchBooks component mockup

---
+++ src/SearchBooks.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import { BookType } from "./Common";
+import BooksGrid from "./BooksGrid";
+
+const SearchBooks = ({ books }: { books: BookType[] }) => {
+  const [query, setQuery] = useState("");
+  return (
+    <div>
+      <div className="search-books-bar">
+        <div className="close-search"></div>
+        <input
+          id="query"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+      </div>
+      <div className="search-books-results">
+        <BooksGrid books={books} />
+      </div>
+    </div>
+  );
+};
+
+export default SearchBooks;
